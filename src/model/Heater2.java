73bb3286b0bec08378a269ca95507861140e96a1
juskis4 <ljuskevicius2002@gmail.com>
2021-03-03 18:22:48 +0100

    Update Heater2.java

--- src/model/Heater2.java
+++ src/model/Heater2.java
@@ -1,4 +1,19 @@
 package model;
 
-public class Heater2 {
+public class Heater2 implements HeaterState{
+
+  @Override public void turnUp(Heater heater)
+  {
+    heater.setState(new Heater3());
+  }
+
+  @Override public void turnDown(Heater heater)
+  {
+    heater.setState(new Heater1());
+  }
+
+  @Override public int getPower()
+  {
+    return 2;
+  }
 }
