5da5c00f4d3d129e8a200baa117d01c08451faa5
Titas212 <45366405+Titas212@users.noreply.github.com>
2021-03-03 18:24:49 +0100

    heater

--- src/model/Heater.java
+++ src/model/Heater.java
@@ -1,4 +1,52 @@
 package model;
 
-public class Heater {
+import utility.observer.subject.UnnamedPropertyChangeSubject;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class Heater implements UnnamedPropertyChangeSubject
+{
+  private HeaterState state;
+  private PropertyChangeSupport propertyChangeSupport;
+
+  public Heater()
+  {
+     state = new Heater0();
+     propertyChangeSupport = new PropertyChangeSupport(this);
+  }
+
+  public void setState(HeaterState state)
+  {
+    this.state = state;
+  }
+
+  public void turnUp()
+  {
+
+  }
+  public void turnDown()
+  {
+
+  }
+
+  public void timeout()
+  {
+
+  }
+
+  public int getPower()
+  {
+    state.getPower();
+  }
+
+  @Override public void addListener(String propertyName, PropertyChangeListener listener)
+  {
+    propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
+  }
+
+  @Override public void removeListener(String propertyName,PropertyChangeListener listener)
+  {
+    propertyChangeSupport.removePropertyChangeListener(propertyName, listener);
+  }
 }
