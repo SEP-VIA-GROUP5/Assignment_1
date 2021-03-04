ba3e4a1feedca8ed8e7419a6a7b3f36c3eda29a7
juskis4 <ljuskevicius2002@gmail.com>
2021-03-04 18:21:00 +0100

    Update ModelManager.java

--- src/model/ModelManager.java
+++ src/model/ModelManager.java
@@ -1,4 +1,69 @@
 package model;
 
-public class ModelManager {
+import java.beans.PropertyChangeListener;
+
+public class ModelManager implements Model {
+  private Heater heater;
+  public ModelManager()
+  {
+    this.heater = new Heater();
+
+  }
+
+  public Heater getHeater()
+  {
+    return heater;
+  }
+
+  @Override public int getPower()
+  {
+    return 0;
+  }
+
+  @Override public void turnUp()
+  {
+
+  }
+
+  @Override public void turnDown()
+  {
+
+  }
+
+  @Override public void setLow(double low)
+  {
+
+  }
+
+  @Override public void setHigh(double high)
+  {
+
+  }
+
+  @Override public double getT0()
+  {
+    return 0;
+  }
+
+  @Override public double getT1()
+  {
+    return 0;
+  }
+
+  @Override public double getT2()
+  {
+    return 0;
+  }
+
+  @Override public void addListener(String propertyName,
+      PropertyChangeListener listener)
+  {
+
+  }
+
+  @Override public void removeListener(String propertyName,
+      PropertyChangeListener listener)
+  {
+
+  }
 }
