bb188e43d20919e4c37b6dd611e6374aa8379881
juskis4 <ljuskevicius2002@gmail.com>
2021-03-04 18:20:58 +0100

    Update Model.java

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,4 +1,15 @@
 package model;
+import utility.observer.subject.NamedPropertyChangeSubject;
 
-public interface Model {
+public interface Model extends NamedPropertyChangeSubject {
+
+  int getPower();
+  void turnUp();
+  void turnDown();
+  void setLow(double low);
+  void setHigh(double high);
+  double getT0();
+  double getT1();
+  double getT2();
+
 }
