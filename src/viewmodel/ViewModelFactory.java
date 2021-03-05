879a7d39d6c48c17605451bfb91eff987477b036
BaicoianuIoanSorin <baicoianuioansorin@gmail.com>
2021-03-05 17:09:44 +0100

    Update ViewModelFactory.java

--- src/viewmodel/ViewModelFactory.java
+++ src/viewmodel/ViewModelFactory.java
@@ -1,4 +1,23 @@
 package viewmodel;
 
+import model.Model;
+
 public class ViewModelFactory {
+    private TemperatureViewModel temperatureViewModel;
+    private LogViewModel logViewModel;
+    private Model model;
+
+    public ViewModelFactory(Model model)
+    {
+        temperatureViewModel = new TemperatureViewModel(model);
+//        logViewModel = new LogViewModel(model);
+    }
+
+    public LogViewModel getLogViewModel() {
+        return logViewModel;
+    }
+
+    public TemperatureViewModel getTemperatureViewModel() {
+        return temperatureViewModel;
+    }
 }
